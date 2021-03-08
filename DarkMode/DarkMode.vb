15ab5e16018e674549f0302da06bef80bf797e72
Ryan Bester <ryan.bester5@gmail.com>
2021-03-08 22:58:57 +0000

    Add dark mode buttons

--- DarkMode/DarkMode.vb
+++ DarkMode/DarkMode.vb
@@ -8,10 +8,12 @@ Imports System.Windows.Forms
 Public Module DarkMode
 
 #Region "Fields"
+
     ''' <summary>
     ''' The build number of the current Windows version.
     ''' </summary>
     Private _buildNumber As UInt32
+
     ''' <summary>
     ''' Whether the dark mode functions have been loaded from the DLL.
     ''' </summary>
@@ -21,6 +23,7 @@ Public Module DarkMode
     ''' Whether the current Windows version supports dark mode.
     ''' </summary>
     Private _darkModeSupported As Boolean = False
+
     ''' <summary>
     ''' Whether dark mode has been enabled by the user. Only applicable if the SYSTEM theme is used.
     ''' </summary>
@@ -35,6 +38,7 @@ Public Module DarkMode
     Private _fnAllowDarkModeForApp As TypeAllowDarkModeForApp = Nothing ' Ordinal 135
     Private _fnSetPreferredAppMode As TypeSetPreferredAppMode = Nothing ' Ordinal 135
     Private _fnIsDarkModeAllowedForWindow As TypeIsDarkModeAllowedForWindow = Nothing ' Ordinal 137
+
 #End Region
 
 #Region "Constants"
@@ -45,9 +49,11 @@ Public Module DarkMode
     Private Const WM_NCACTIVATE = &H86
 
     Private Const WCA_USEDARKMODECOLORS = 26
+
 #End Region
 
 #Region "Enumerations"
+
     Public Enum Theme
         SYSTEM
         DARK
@@ -66,66 +72,90 @@ Public Module DarkMode
         IHCM_USE_CACHED_VALUE
         IHCM_REFRESH
     End Enum
+
 #End Region
 
 #Region "Structures"
+
     <StructLayout(LayoutKind.Sequential)>
     Private Structure WinCompositionAttrData
         Public Attribute As UInt32
         Public Data As IntPtr
         Public SizeOfData As UInt64
     End Structure
+
 #End Region
 
 #Region "DLL Imports"
-    <DllImport("kernel32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+
+    <DllImport("kernel32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
     Private Function LoadLibrary(ByVal lpLibFileName As String) As IntPtr
     End Function
 
-    <DllImport("kernel32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    <DllImport("kernel32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
+    Private Function LoadLibraryEx(ByVal lpLibFileName As String, hFile As IntPtr, dwFlags As UInt32) As IntPtr
+    End Function
+
+    <DllImport("kernel32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
     Private Function FreeLibrary(ByVal hLibModule As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
     End Function
 
-    <DllImport("kernel32.dll", SetLastError:=True)>
+    <DllImport("kernel32.dll", SetLastError := True)>
     Private Function GetProcAddress(ByVal hModule As IntPtr, lpProcName As UInt32) As IntPtr
     End Function
 
-    <DllImport("kernel32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    <DllImport("kernel32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
     Private Function CallWindowProc(ByVal hModule As IntPtr, lpProcName As String) As IntPtr
     End Function
 
-    <DllImport("ntdll.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    <DllImport("ntdll.dll", CharSet := CharSet.Unicode, SetLastError := True)>
     Private Sub RtlGetNtVersionNumbers(ByRef major As UInt32, ByRef minor As UInt32, ByRef build As UInt32)
     End Sub
 
-    <DllImport("user32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
-    Private Function SetWindowCompositionAttribute(ByVal hWnd As IntPtr, ByRef data As WinCompositionAttrData) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    <DllImport("user32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
+    Private Function SetWindowCompositionAttribute(ByVal hWnd As IntPtr, ByRef data As WinCompositionAttrData) _
+        As <MarshalAs(UnmanagedType.Bool)> Boolean
     End Function
 
-    <DllImport("user32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
-    Private Function SetProp(ByVal hWnd As IntPtr, ByVal lpString As String, ByVal hData As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    <DllImport("user32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
+    Private Function SetProp(ByVal hWnd As IntPtr, ByVal lpString As String, ByVal hData As IntPtr) _
+        As <MarshalAs(UnmanagedType.Bool)> Boolean
     End Function
 
     ' Assumes wParam is on 32 bit. Different on 16 bit and 64 bit.
-    <DllImport("user32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
-    Private Function SendMessage(ByVal hWnd As IntPtr, ByVal msg As UInt32, ByVal wParam As Int32, ByVal lParam As Int32) As Int32
+    <DllImport("user32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
+    Private Function SendMessage(ByVal hWnd As IntPtr, ByVal msg As UInt32, ByVal wParam As Int32, ByVal lParam As Int32) _
+        As Int32
     End Function
 
-    <DllImport("uxtheme.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    <DllImport("user32.dll", CharSet := CharSet.Unicode, SetLastError := True)>
+    Private Function UpdateWindow(ByVal hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    <DllImport("uxtheme.dll", CharSet := CharSet.Unicode, SetLastError := True)>
     Private Function SetWindowTheme(ByVal hWnd As IntPtr, pszSubAppName As String, pszSubIdList As String) As Int32
     End Function
 
-    Private Delegate Function TypeOpenNcThemeData(ByVal hWnd As IntPtr, ByVal pszClassList As String) As IntPtr ' Ordinal 49
+    Private Delegate Function TypeOpenNcThemeData(ByVal hWnd As IntPtr, ByVal pszClassList As String) As IntPtr _
+    ' Ordinal 49
     Private Delegate Sub TypeRefreshImmersiveColorPolicyState() ' Ordinal 104
-    Private Delegate Function TypeGetIsImmersiveColorUsingHighContrast(mode As ImmersiveHCCacheMode) As <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 106
-    Private Delegate Function TypeShouldAppsUseDarkMode() As <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 132
-    Private Delegate Function TypeAllowDarkModeForWindow(ByVal hWnd As IntPtr, allow As Boolean) As <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 133
-    Private Delegate Function TypeAllowDarkModeForApp(<MarshalAs(UnmanagedType.Bool)> allow As Boolean) As <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 135
-    Private Delegate Function TypeSetPreferredAppMode(ByVal appMode As PreferredAppMode) As PreferredAppMode ' Ordinal 135
-    Private Delegate Function TypeIsDarkModeAllowedForWindow(ByVal hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 137
+    Private Delegate Function TypeGetIsImmersiveColorUsingHighContrast(mode As ImmersiveHCCacheMode) As _
+        <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 106
+    Private Delegate Function TypeShouldAppsUseDarkMode() As <MarshalAs(UnmanagedType.I1)> Boolean ' Ordinal 132
+    Private Delegate Function TypeAllowDarkModeForWindow _
+        (ByVal hWnd As IntPtr, <MarshalAs(UnmanagedType.I1)> allow As Boolean) As _
+        <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 133
+    Private Delegate Function TypeAllowDarkModeForApp(<MarshalAs(UnmanagedType.Bool)> allow As Boolean) As _
+        <MarshalAs(UnmanagedType.Bool)> Boolean ' Ordinal 135
+    Private Delegate Function TypeSetPreferredAppMode(ByVal appMode As PreferredAppMode) As PreferredAppMode _
+    ' Ordinal 135
+    Private Delegate Function TypeIsDarkModeAllowedForWindow(ByVal hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> _
+        Boolean ' Ordinal 137
+
 #End Region
 
 #Region "Methods"
+
     ''' <summary>
     ''' Convert two WORDs into a DWORD.
     ''' </summary>
@@ -143,10 +173,10 @@ Public Module DarkMode
     ''' <returns>True if the Windows version supports dark mode, or false if it doesn't.</returns>
     Private Function CheckBuildNumber(buildNumber As UInt32) As Boolean
         Return _
-            buildNumber = 17763 Or ' Version 1809
-            buildNumber = 18362 Or ' Version 1903
-            buildNumber = 18363 Or ' Version 1909
-            buildNumber = 19041 Or ' Version 2004
+            buildNumber = 17763 Or ' Version 1809 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+            buildNumber = 18362 Or ' Version 1903 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+            buildNumber = 18363 Or ' Version 1909 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+            buildNumber = 19041 Or ' Version 2004 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
             buildNumber = 19042    ' Version 2010
     End Function
 
@@ -159,7 +189,7 @@ Public Module DarkMode
         Dim dark As Integer = 0
 
         If theme = Theme.SYSTEM _
-            Or theme = Theme.DARK Then
+           Or theme = Theme.DARK Then
 
             If theme = Theme.SYSTEM Then
                 If _fnIsDarkModeAllowedForWindow IsNot Nothing Then
@@ -178,10 +208,12 @@ Public Module DarkMode
         If (_buildNumber < 18362) Then
             SetProp(hWnd, "UseImmersiveDarkModeColors", dark)
         Else
-            Dim size = Marshal.SizeOf(Of Integer)
+            Dim size = Marshal.SizeOf (Of Integer)
             Dim ptr As IntPtr = Marshal.AllocHGlobal(size)
             Marshal.WriteInt32(ptr, dark)
-            SetWindowCompositionAttribute(hWnd, New WinCompositionAttrData() With {.Attribute = WCA_USEDARKMODECOLORS, .Data = ptr, .SizeOfData = size})
+            SetWindowCompositionAttribute(hWnd,
+                                          New WinCompositionAttrData() _
+                                             With {.Attribute = WCA_USEDARKMODECOLORS, .Data = ptr, .SizeOfData = size})
             Marshal.FreeHGlobal(ptr)
         End If
     End Sub
@@ -212,7 +244,7 @@ Public Module DarkMode
 
             If major = 10 And minor = 0 And CheckBuildNumber(_buildNumber) Then
                 ' Load uxtheme.dll. We cannot use DllImport here since the symbol is version specific.
-                Dim libPtr As IntPtr = LoadLibrary("uxtheme.dll")
+                Dim libPtr As IntPtr = LoadLibraryEx("uxtheme.dll", Nothing, &h800)
                 If libPtr = 0 Then
                     ' Error loading library
                     Return False
@@ -222,52 +254,59 @@ Public Module DarkMode
                 Dim pa49 As IntPtr = GetProcAddress(libPtr, MakeDWORD(49, 0)) ' Ordinal 49
                 If pa49 <> 0 Then
                     ' Store pointer to function 
-                    _fnOpenNcThemeData = Marshal.GetDelegateForFunctionPointer(Of TypeOpenNcThemeData)(pa49)
+                    _fnOpenNcThemeData = Marshal.GetDelegateForFunctionPointer (Of TypeOpenNcThemeData)(pa49)
                 End If
 
                 Dim pa104 As IntPtr = GetProcAddress(libPtr, MakeDWORD(104, 0)) ' Ordinal 104
                 If pa104 <> 0 Then
-                    _fnRefreshImmersiveColorPolicyState = Marshal.GetDelegateForFunctionPointer(Of TypeRefreshImmersiveColorPolicyState)(pa104)
+                    _fnRefreshImmersiveColorPolicyState =
+                        Marshal.GetDelegateForFunctionPointer (Of TypeRefreshImmersiveColorPolicyState)(pa104)
                 End If
 
                 Dim pa106 As IntPtr = GetProcAddress(libPtr, MakeDWORD(106, 0)) ' Ordinal 106
                 If pa106 <> 0 Then
-                    _fnGetIsImmersiveColorUsingHighContrast = Marshal.GetDelegateForFunctionPointer(Of TypeGetIsImmersiveColorUsingHighContrast)(pa106)
+                    _fnGetIsImmersiveColorUsingHighContrast =
+                        Marshal.GetDelegateForFunctionPointer (Of TypeGetIsImmersiveColorUsingHighContrast)(pa106)
                 End If
 
                 Dim pa132 As IntPtr = GetProcAddress(libPtr, MakeDWORD(132, 0)) ' Ordinal 132
                 If pa132 <> 0 Then
-                    _fnShouldAppsUseDarkMode = Marshal.GetDelegateForFunctionPointer(Of TypeShouldAppsUseDarkMode)(pa132)
+                    _fnShouldAppsUseDarkMode =
+                        Marshal.GetDelegateForFunctionPointer (Of TypeShouldAppsUseDarkMode)(pa132)
                 End If
 
                 Dim pa133 As IntPtr = GetProcAddress(libPtr, MakeDWORD(133, 0)) ' Ordinal 133
                 If pa133 <> 0 Then
-                    _fnAllowDarkModeForWindow = Marshal.GetDelegateForFunctionPointer(Of TypeAllowDarkModeForWindow)(pa133)
+                    _fnAllowDarkModeForWindow =
+                        Marshal.GetDelegateForFunctionPointer (Of TypeAllowDarkModeForWindow)(pa133)
                 End If
 
                 Dim pa135 As IntPtr = GetProcAddress(libPtr, MakeDWORD(135, 0)) ' Ordinal 135
                 If pa135 <> 0 Then
                     ' < 1903
                     If build < 18362 Then
-                        _fnAllowDarkModeForApp = Marshal.GetDelegateForFunctionPointer(Of TypeAllowDarkModeForApp)(pa135)
+                        _fnAllowDarkModeForApp =
+                            Marshal.GetDelegateForFunctionPointer (Of TypeAllowDarkModeForApp)(pa135)
                     Else
-                        _fnSetPreferredAppMode = Marshal.GetDelegateForFunctionPointer(Of TypeSetPreferredAppMode)(pa135)
+                        _fnSetPreferredAppMode =
+                            Marshal.GetDelegateForFunctionPointer (Of TypeSetPreferredAppMode)(pa135)
                     End If
                 End If
 
                 Dim pa137 As IntPtr = GetProcAddress(libPtr, MakeDWORD(137, 0)) ' Ordinal 137
                 If pa137 <> 0 Then
-                    _fnIsDarkModeAllowedForWindow = Marshal.GetDelegateForFunctionPointer(Of TypeIsDarkModeAllowedForWindow)(pa137)
+                    _fnIsDarkModeAllowedForWindow =
+                        Marshal.GetDelegateForFunctionPointer (Of TypeIsDarkModeAllowedForWindow)(pa137)
                 End If
 
                 FreeLibrary(libPtr)
 
                 If _fnOpenNcThemeData IsNot Nothing _
-                    And _fnRefreshImmersiveColorPolicyState IsNot Nothing _
-                    And _fnShouldAppsUseDarkMode IsNot Nothing _
-                    And _fnAllowDarkModeForWindow IsNot Nothing _
-                    And (_fnAllowDarkModeForApp IsNot Nothing Or _fnSetPreferredAppMode IsNot Nothing) _
-                    And _fnIsDarkModeAllowedForWindow IsNot Nothing Then
+                   And _fnRefreshImmersiveColorPolicyState IsNot Nothing _
+                   And _fnShouldAppsUseDarkMode IsNot Nothing _
+                   And _fnAllowDarkModeForWindow IsNot Nothing _
+                   And (_fnAllowDarkModeForApp IsNot Nothing Or _fnSetPreferredAppMode IsNot Nothing) _
+                   And _fnIsDarkModeAllowedForWindow IsNot Nothing Then
 
                     _darkModeSupported = True
                     _darkModeEnabled = _fnShouldAppsUseDarkMode() And Not SystemInformation.HighContrast
@@ -280,6 +319,25 @@ Public Module DarkMode
 
         Return True
     End Function
+
+    Private Sub SetButtonStyle(btn As Button)
+        If Not InitDarkMode()
+            Return
+        End If
+
+        If _darkModeEnabled
+            SetWindowTheme(btn.Handle, "DarkMode_Explorer", Nothing)
+        Else
+            SetWindowTheme(btn.Handle, "Explorer", Nothing)
+        End If
+
+        If _fnAllowDarkModeForWindow IsNot Nothing Then
+            _fnAllowDarkModeForWindow(btn.Handle, True)
+        End If
+
+        SendMessage(btn.Handle, WM_THEMECHANGED, 0, 0)
+        btn.Invalidate()
+    End Sub
 
     Public Sub SetAppTheme(theme As Theme)
         If Not InitDarkMode() Then
@@ -324,11 +382,27 @@ Public Module DarkMode
                 End If
 
                 RefreshTitleBarThemeColour(m.HWnd, theme)
+
+                ' Enable dark mode for controls
+                For Each control As Control in window.Controls
+                    If control.GetType() Is GetType(Button)
+                        ' Theme button
+                        SetButtonStyle(CType(control, Button))
+                    End If
+                Next
             Case WM_SETTINGCHANGED
                 If IsColourSchemeChangeMessage(m.LParam) Then
                     _darkModeEnabled = _fnShouldAppsUseDarkMode() And Not SystemInformation.HighContrast
 
                     RefreshTitleBarThemeColour(window.Handle, theme)
+
+                    ' Update control theme
+                    For Each control As Control In window.Controls
+                        If control.GetType() Is GetType(Button)
+                            ' Theme button
+                            SetButtonStyle(CType(control, Button))
+                        End If
+                    Next
                 End If
         End Select
     End Sub
@@ -340,6 +414,6 @@ Public Module DarkMode
         SendMessage(window.Handle, WM_NCACTIVATE, 0, 0)
         SendMessage(window.Handle, WM_NCACTIVATE, 1, 0)
     End Sub
-#End Region
 
+#End Region
 End Module
